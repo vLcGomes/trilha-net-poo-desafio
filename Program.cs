75d0114cacb48b916b9174e26e89f8801a518a9b
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2024-08-23 20:45:15 -0300

    Projeto Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone i1 = new("8333-1523", "10", "1526895", 128);
+Nokia n1 = new("9444-1621", "7", "187360", 256);
+
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Facebook");
+
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("WhatsApp");
